d09f96f0e48e4f779abdcdbc482cf506dddafc48
SimonQin611 <1042537939@qq.com>
2020-06-08 15:54:18 +0800

    删除小活动

--- cloud-activity/src/main/java/com/arthur/cloud/activity/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.arthur.cloud.activity.mapper;
-
-import com.arthur.cloud.activity.model.User;
-import com.arthur.cloud.activity.utils.MyMapper;
-
-public interface UserMapper extends MyMapper<User> {
-}
